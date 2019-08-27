0ea793345751b03b59b76cd70bddd9ba881dcd5c
Phumsan <52192362+Phumsan@users.noreply.github.com>
2019-08-27 20:22:54 +0700

    Add files via upload

---
+++ functionrandom.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int checkNum(int,int);
+int main()
+{	
+	int num,num1,c=0;
+	cout<<"###Welcome to guessing number game###\n";
+	cout<<"Secret number has been chosen\n";
+	srand(time(NULL));
+	num1=1+rand()%10;
+	do{
+	cout<<"Guess the number(1 to 10): ";
+	cin>>num;
+	checkNum(num,num1);
+	c=c+1;
+	}while(num!=num1);
+	cout<<"You made "<<" "<<c<<" "<<" guesses "<<endl;
+	return(0);
+}
+int checkNum(int n,int n1)
+{
+	if(n<n1)
+	{
+		cout<<"The secret number is lower\n";
+	}
+	else if(n>n1)
+	{
+		cout<<"The secret number is higher\n";
+	}
+	else if(n==n1)
+	{
+		cout<<"Congratulations!\n";
+		cout<<"The secret number is"<<" "<<n1<<endl;
+	}
+	
+	return(n,n1);
+}
